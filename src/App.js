ade677cdab1b89d1c2077edf4811942579796624
Leandro <lterrado@gmail.com>
2019-11-23 21:38:41 -0300

    adding Rank

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 
 import './App.css';
+import Navigation from "./components/Navigation/Navigation"
+import Logo from "./components/Logo/Logo"
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkform"
+import Rank from "./components/Rank/Rank"
+import "tachyons";
 
 function App() {
   return (
     <div className="App">
-
+      <Navigation />
+      <Logo />
+      <Rank />
+      <ImageLinkForm />
+      {/* <FaceRecognition /> */}
     </div>
   );
 }
