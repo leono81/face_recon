d03a3fa71e8ecf0b2e603ee9febb50f0787806fa
Leandro Terrado <lterrado@gmail.com>
2020-02-27 16:07:42 -0300

    add a placeholder to input object

--- src/components/ImageLinkForm/ImageLinkform.js
+++ src/components/ImageLinkForm/ImageLinkform.js
@@ -13,6 +13,7 @@ const imageLinkForm = ({onInputChange, onButtonSubmit}) =>{
                   className="f4 pa2 w-70 center" 
                   type="text" 
                   onChange={onInputChange}
+                  placeholder="insert image URL ..."
                />
                <button 
                   className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple" 
